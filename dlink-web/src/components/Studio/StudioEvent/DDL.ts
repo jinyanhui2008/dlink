36b291f793bc8c2250d6209cb704f3467d1a09a2
ZhangZifeng <>
2022-09-23 17:27:05 +0800

    fixed bug

--- dlink-web/src/components/Studio/StudioEvent/DDL.ts
+++ dlink-web/src/components/Studio/StudioEvent/DDL.ts
@@ -22,7 +22,7 @@ import {executeDDL, getMSCatalogs, isUsingDS} from "@/pages/DataStudio/service";
 import FlinkSQL from "./FlinkSQL";
 import {MetaStoreCatalogType, SessionType, TaskType} from "@/pages/DataStudio/model";
 import {message, Modal} from "antd";
-import {addOrUpdateData, getData, handleRemove, postAll} from "@/components/Common/crud";
+import {addOrUpdateData, CODE, getData, handleRemove, postAll} from "@/components/Common/crud";
 
 /*--- 保存sql ---*/
 export function saveTask(current: any, dispatch: any) {
@@ -162,6 +162,7 @@ export function showSessionCluster(dispatch: any) {
     });
   });
 }
+
 /*--- 刷新 Session集群 ---*/
 export function getDolphinSchduleAvailable(dispatch: any) {
   const res = isUsingDS();
@@ -237,9 +238,10 @@ export function showAlertGroup(dispatch: any) {
 export function showMetaDataTable(id: number) {
   return getData('api/database/getSchemasAndTables', {id: id});
 }
+
 /*--- 刷新 数据表样例数据 ---*/
-export function showTableData(id: number,schemaName:String,tableName:String,option:{}) {
-  return postAll('api/database/queryData', {id: id,schemaName:schemaName,tableName:tableName,option:option});
+export function showTableData(id: number, schemaName: String, tableName: String, option: {}) {
+  return postAll('api/database/queryData', {id: id, schemaName: schemaName, tableName: tableName, option: option});
 }
 
 /*--- 刷新 Flink Jobs ---*/
